d32495feed5f82c3e7fbfd96c9d1eb996c3e3491
dharmand <denis.harmand@gmail.com>
2013-10-24 10:20:51 +0200

    Premier test

---
+++ src/hello_world.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   hello_world.cpp
+ * Author: Asus
+ *
+ * Created on 23 octobre 2013, 12:47
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+    cout<<"hello world"<<endl;
+    return 0;
+}
+
